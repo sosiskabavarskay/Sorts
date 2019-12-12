dc9a2e7c108545eb67b29bd40131a049206bd02b
Максим Самусин <maks_8342@hotmail.com>
2019-12-12 19:25:08 +0500

    Готово, все пашет, не зря в мастер сру

--- Sorts/Sorts.cpp
+++ Sorts/Sorts.cpp
@@ -59,36 +59,101 @@ double shake(double* arr) {
 	return result;
 }
 
-//void merge(double* arr) {
-//	int len = lenarr(arr);
-//	int left = len / 2;
-//	int rigth = len - left;
-//	double* tmp_left_arr;
-//	double* tmp_rigth_arr;
-//	tmp_left_arr = new double[left];
-//	tmp_rigth_arr = new double[rigth];
-//}
+double shake1(double* mas)
+{
+	int size = lenarr(mas);
+	double start_time = clock();
+	int L = 1, R = size - 1;
+	while (L <= R)
+	{
+		for (int i = R; i >= L; i--)
+		{
+			if (mas[i - 1] > mas[i])
+			{
+				double x = mas[i];
+				mas[i] = mas[i - 1];
+				mas[i - 1] = x;
+			}
+		}
+		L++;
+
+		for (int i = L; i <= R; i++)
+		{
+			if (mas[i - 1] > mas[i])
+			{
+				double x = mas[i];
+				mas[i] = mas[i - 1];
+				mas[i - 1] = x;
+			}
+		}
+		R--;
+	}
+
+	double end_time = clock();
+	double time = (end_time - start_time) / CLOCKS_PER_SEC;
+	return time;
+}
+
+double merge(double* mas)
+{
+	int size = lenarr(mas);
+	double start_time = clock();
+	int step = 1;  
+	int* temp = new int[size]; 
+	while (step < size)  
+	{
+		int index = 0;    
+		int l = 0;      
+		int m = l + step;  
+		int r = l + step * 2;  
+		do
+		{
+			m = m < size ? m : size;  
+			r = r < size ? r : size;
+			int i1 = l, i2 = m; 
+			for (; i1 < m && i2 < r; ) 
+			{
+				if (mas[i1] < mas[i2]) { temp[index++] = mas[i1++]; } 
+				else { temp[index++] = mas[i2++]; }
+			}
+			while (i1 < m) temp[index++] = mas[i1++]; 
+			while (i2 < r) temp[index++] = mas[i2++]; 
+			l += step * 2; 
+			m += step * 2;
+			r += step * 2;
+		} while (l < size); 
+		for (int i = 0; i < size; i++) 
+			mas[i] = temp[i];
+		step *= 2; 
+	}
+	double end_time = clock(); 
+	double time = (end_time - start_time) / CLOCKS_PER_SEC;
+	return time;
+}
 
 int main()
 {
 	setlocale(0, "");
 	srand(time(NULL));
-	double time_ch = 0, time_bu = 0, time_sh = 0;
+	double time_ch = 0, time_bu = 0, time_sh = 0, time_me = 0;
 	ofstream out;
 	out.open("Text.txt");
 	double n = 10;
-	for (int num = 1000, cntr = 0; num <= 5000; num += 500, cntr++) {
+	for (int num = 50000, cntr = 0; num <= 200000; num += 50000, cntr++) {
 		double* arr;
 		arr = new double[num];
+		double* arr1;
 		for (int i = 1; i <= n; i++) {
 			for (int i = 0; i < num; i++) arr[i] = double(rand()) / double(10 * rand() % 1000); time_ch += choise(arr);
 			for (int i = 0; i < num; i++) arr[i] = double(rand()) / double(10 * rand() % 1000); time_bu += bubble(arr);
-			for (int i = 0; i < num; i++) arr[i] = double(rand()) / double(10 * rand() % 1000); time_sh += shake(arr);
+			for (int i = 0; i < num; i++) arr[i] = double(rand()) / double(10 * rand() % 1000); time_sh += shake1(arr);
+			for (int i = 0; i < num; i++) arr[i] = double(rand()) / double(10 * rand() % 1000); time_me += merge(arr);
 		}
 		cout << "Среднее время сортировки выбором для " << num << " элементов: " << time_ch / n << endl;
 		cout << "Среднее время сортировки пузырьком для " << num << " элементов: " << time_bu / n << endl;
 		cout << "Среднее время сортировки шейкером для " << num << " элементов: " << time_sh / n << endl;
-		out << num << " " << time_ch / n << " " << time_bu / n << " " << time_sh / n << endl;
+		cout << "Среднее время сортировки слиянием для " << num << " элементов: " << time_me / n << endl;
+		out << num << " " << time_ch / n << " " << time_bu / n << " " << time_sh / n << " " << time_me / n << endl;
 		delete[] arr;
 	}
 	out.close();
